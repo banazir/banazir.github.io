794199dbb4a750be253c526a4f6cd0e00c12ba6c
banazir <bhsu@ksu.edu>
2025-11-16 18:42:58 +0000

    Deploying to gh-pages from @ banazir/banazir.github.io@a9eb4d394decf5ae133dc93765fd94ef7bfd019f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%68%73%75@%6B%73%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -522,14 +522,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=JQoJRcIAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.kddresearch.org", "_blank");
         },
       },{
       id: 'light-theme',
